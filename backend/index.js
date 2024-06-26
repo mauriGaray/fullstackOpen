ff71111489552f7bf4be30952b2d2f9a66487b32
mauriGaray <garaycarlosmauricio17@gmail.com>
2024-06-26 12:21:39 -0300

    3.13

--- backend/index.js
+++ backend/index.js
@@ -3,16 +3,9 @@ const app = express();
 require("dotenv").config();
 const morgan = require("morgan");
 const cors = require("cors");
+const PersonsModel = require("./models/mongo");
 
 const PORT = process.env.PORT || 3000;
-
-// Base de datos simulada
-let persons = [
-  { id: 1, name: "Arto Hellas", number: "040-123456" },
-  { id: 2, name: "Ada Lovelace", number: "39-44-5323523" },
-  { id: 3, name: "Dan Abramov", number: "12-43-234345" },
-  { id: 4, name: "Mary Poppendieck", number: "39-23-6423122" },
-];
 
 // Configuración de middlewares
 app.use(express.static("dist"));
---
+++ backend/models/mongo.js
@@ -0,0 +1,29 @@
+// Instanciación  de mongoose
+const mongoose = require("mongoose");
+mongoose.set("strictQuery", false);
+//Configuración de la URL
+const url = process.env.MONGODB_URI;
+console.log("Connecting to ", url);
+
+//Conexión a MongoDB
+mongoose
+  .connect(url)
+  .then((res) => console.log("connected to DB"))
+  .catch((err) => console.log(err));
+
+//Creación de Schema
+const PersonSchema = new mongoose.Schema({
+  name: String,
+  number: String,
+  id: String,
+});
+
+PersonSchema.set("toJSON", {
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString();
+    delete returnedObject._id;
+    delete returnedObject.__v;
+  },
+});
+
+module.exports = mongoose.model("Persons", PersonSchema);
